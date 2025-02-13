724463504ba963aa890c92f401df3cc44d07d185
mihailohranisavljevic <mihailoh@stud.ntnu.no>
2025-02-13 23:23:47 +0100

    Currency record implemented

--- src/main/java/Currency.java
+++ src/main/java/Currency.java
@@ -1,2 +1,20 @@
-public class Currency {
+/**
+ * A class to represent a currency.
+ * Each currency has a name, a code, and a symbol.
+ *
+ * @author Mihailo Hranisavljevic
+ * @version 2025.02.13
+ */
+
+public record Currency(String name, String code, String symbol) {
+
+  /**
+   * Returns a string representation of the currency.
+   *
+   * @return A string with currency details.
+   */
+  @Override
+  public String toString() {
+    return name + " (" + code + ") " + symbol;
+  }
 }
